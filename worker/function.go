3c9b347c103993f8d7a2c6b3ffeb7fb592f08cd2
aljesusg <aljesusg@MBP-de-aljesusg.technicolor.net>
2022-02-12 08:29:04 +0100

    Test

--- worker/function.go
+++ worker/function.go
@@ -2,15 +2,15 @@ package function
 
 import (
 	"fmt"
+	db "github.com/grownity/grownity/db"
+	git "github.com/grownity/grownity/github"
 	"net/http"
-	//git "github.com/grownity/grownity/github"
-	//db "github.com/grownity/grownity/db"
 )
 
 // HelloHTTP is an HTTP Cloud Function with a request parameter.
 func GrownityOn(w http.ResponseWriter, r *http.Request) {
-	//	db.InitDB()
-	//	database := db.GetClient()
-	//	database.UpdateOrg(git.GetOrganization())
+	db.InitDB()
+	database := db.GetClient()
+	database.UpdateOrg(git.GetOrganization())
 	fmt.Fprint(w, "Hello, World!")
 }
--- worker/go.mod
+++ worker/go.mod
@@ -1,9 +1,5 @@
-module github.com/grownity/grownity/worker
+module grownity.com/worker/m
 
 go 1.16
 
-require (
-	github.com/grownity/grownity v0.0.0-20220205074249-7a028689c110
-	google.golang.org/api v0.57.0 // indirect
-	google.golang.org/genproto v0.0.0-20210921142501-181ce0d877f6 // indirect
-)
+require github.com/grownity/grownity v0.0.0-20220212062903-0cd0893f6eaa
