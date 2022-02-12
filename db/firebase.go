d5bf0cc5df2f3a36352db21a731d41be84ee9705
aljesusg <aljesusg@MBP-de-aljesusg.technicolor.net>
2022-02-12 11:52:22 +0100

    Set tests

--- db/firebase.go
+++ db/firebase.go
@@ -2,9 +2,10 @@ package db
 
 import (
 	"context"
-	"github.com/google/go-github/v41/github"
 	"log"
 	"os"
+
+	"github.com/google/go-github/v41/github"
 
 	firebase "firebase.google.com/go/v4"
 	firebaseDb "firebase.google.com/go/v4/db"
@@ -15,13 +16,22 @@ type FbInterface interface {
 	UpdateOrg(organization github.Organization) error
 }
 
-func FirebaseClient() (*firebaseDb.Client, error) {
+func FirebaseClient(url string, account string) (*firebaseDb.Client, error) {
 	ctx := context.Background()
-	config := &firebase.Config{
-		DatabaseURL: os.Getenv("DB_URL"),
+	databaseUrl := url
+	var json_acc []byte
+	if databaseUrl == "" {
+		databaseUrl = os.Getenv("DB_URL")
 	}
-	json := []byte(os.Getenv("FB_SERVICE_ACCOUNT"))
-	opt := option.WithCredentialsJSON(json)
+	if account == "" {
+		json_acc = []byte(os.Getenv("FB_SERVICE_ACCOUNT"))
+	} else {
+		json_acc = []byte(account)
+	}
+	config := &firebase.Config{
+		DatabaseURL: databaseUrl,
+	}
+	opt := option.WithCredentialsJSON(json_acc)
 	app, err := firebase.NewApp(ctx, config, opt)
 	if err != nil {
 		log.Fatal(err)
